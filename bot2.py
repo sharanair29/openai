6cae98fa1ffe4522dfe7a365fd26cb19d792e8f9
sharanair29 <sharanair29@gmail.com>
2022-06-27 22:00:05 +0800

    openai

---
+++ bot2.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from random import choice
+from flask import Flask, request
+import os
+import openai
+
+load_dotenv()
+openai.api_key = "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"
+completion = openai.Completion()
+
+start_sequence = "\nBot:"
+restart_sequence = "\n\nPerson:"
+session_prompt = "Hey there! I am AiVA."
+
+def ask(question, chat_log=None):
+    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt= prompt_text,
+        temperature = 0.9,
+        max_tokens = 150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.3,
+        stop=["\n"],
+    )
+    story = response['choices'][0]['text']
+    return str(story)
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
